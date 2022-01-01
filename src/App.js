352ea0c0a936531bfb258393a718e76d805ce200
etedali <etedali@gmail.com>
2022-01-01 18:58:50 -0500

    added arow function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -33,6 +33,6 @@ function App() {
       <Expenses expenses={expenses}></Expenses>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,7 @@
-import './Card.css'
-function Card(props) {
-    const classes = 'card ' + props.className;
-    return <div className={classes}>{props.children}</div>
-}
-
+import "./Card.css";
+const Card = (props) => {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+};
 
 export default Card;
